83d7c90428000243b45032df34a7621b8d88c2b2
the_lucifer <mmshivesh@gmail.com>
2019-02-02 12:39:44 +0530

    Added pickle caching and argparsing

--- scraper.py
+++ scraper.py
@@ -1,27 +1,48 @@
 from bs4 import BeautifulSoup
-from pprint import pprint
+import argparse
 import requests
 import json
 import pickle
 
-url = 'https://www.google.com/search?tbm=isch&q='
-search_string = input("Enter a search term : ")
-search_string=search_string.replace(' ', '+')
-search_url = url + search_string
-print(f"The search url is : {search_url}")
+parser = argparse.ArgumentParser()
 
-downloadurls = []
+parser.add_argument('-v',action='store_true', default=False, help='Debug/verbose mode')
+parser.add_argument('-c',action='store_true', default=False, help='Cache images offline in a pickle object')
+parser.add_argument('-s',dest='search_string', default=None, help='Search string to query')
+parser.add_argument('-d',dest='download_location',default='./', help='Image download location')
+parser.add_argument('-p',dest='pickle_location', default=None, help='Pickle object location')
+args = parser.parse_args()
+if args.v:
+	print(args)
 
-response = requests.get(search_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15'})#, headers={'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0"})
+if args.pickle_location==None:
+	url = 'https://www.google.com/search?tbm=isch&q='
+	if args.search_string==None:
+		args.search_string = input("Enter a search query : ")
+	search_string=args.search_string.replace(' ', '+')
+	search_url = url + search_string
+	if args.v:
+		print(f"The search url is : {search_url}")
 
-text_soup = BeautifulSoup(response.content, "html.parser")
-allimages = text_soup.find_all('div', {'class':'rg_meta'})
+	downloadurls = []
 
-for i in allimages:
-	downloadurls.append(json.loads(i.text)['ou'])
+	response = requests.get(search_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15'})
 
-print("pickling query to file...")
-fname = search_string.replace('+','_')
-fp = open(f'{fname}','wb')
-pickle.dump(downloadurls, fname)
-fp.close()
+	text_soup = BeautifulSoup(response.content, "html.parser")
+	allimages = text_soup.find_all('div', {'class':'rg_meta'})
+
+	for i in allimages:
+		downloadurls.append(json.loads(i.text)['ou'])
+	print(f'Obtained {len(downloadurls)} image urls')
+
+	if args.c:
+		fname = search_string.replace('+','_')
+		print(f"caching query to file: {fname}...")
+		fp = open(fname,'wb')
+		pickle.dump(downloadurls, fp)
+		fp.close()
+else:
+	fp = open(args.pickle_location,'rb')
+	links = pickle.load(fp)
+	if args.v:
+		print(links)
